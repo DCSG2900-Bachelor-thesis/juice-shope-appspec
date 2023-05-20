8f45cedae64dd022f752025b456b857bf1f9b431
SebastianHestsveen <70824743+SebastianHestsveen@users.noreply.github.com>
2023-05-20 17:28:47 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /home/ec2-user/.bash_profile
+cd /home
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+sudo su
+npm install -y
+npm update -y
+cd /home/scripts
+chmod +x start.sh
